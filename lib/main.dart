6a2d70eab7b3c17b86d795ead8dbec226fd873d3
ikuma-hiroyuki <chemicalbondo@gmail.com>
2020-10-08 12:57:57 +0900

    ExpandeどラップされたImega Widget作成

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,23 @@ class DicePage extends StatelessWidget {
     return Center(
       child: Row(
         children: [
-          Expanded(child: Image.asset('images/minion.png')),
-          Expanded(child: Image.asset('images/linux.png')),
+          ExpandedImage('images/minion.png'),
+          ExpandedImage('images/mac.png'),
         ],
+      ),
+    );
+  }
+}
+
+class ExpandedImage extends StatelessWidget {
+  final String path;
+  ExpandedImage(this.path);
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+        child: Image.asset(path),
+        onPressed: () => print('Hello'),
       ),
     );
   }
